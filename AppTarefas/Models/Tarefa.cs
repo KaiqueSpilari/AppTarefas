8a6b0194f19bc42d1f83072277ce00a38ed2178a
KaiqueSpilari <spilarisantos@gmail.com>
2025-08-08 16:41:41 -0300

    Adiciona funcionalidade de lista de tarefas
    - Atualiza `_Layout.cshtml`:
      - Altera atributo `lang` para `""`.
      - Ajusta formatação e adiciona link para "Lista de tarefa".
    - Adiciona `TarefasController` com ação `Index`.
    - Cria modelo `Tarefa` com propriedades `TarefaId`, `Título`,
      `Descrição` e `Concluida`.
    - Adiciona view `Index.cshtml` para exibir lista de tarefas.

---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        // Nome da chave primária deve ser Nome da Classe + "Id"
+        public int TarefaId { get; set; } // Id é a chave primária
+        public string? Título { get; set; }
+        public string? Descrição { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
